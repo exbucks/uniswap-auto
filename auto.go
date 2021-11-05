fbd64dd5de78464280b3273fc95c85a5dfd9d4c8
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-05 18:12:42 +0900

    Initial commit

---
+++ auto.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ethPrice := map[string]string{
+		"query": `
+	        query bundles {
+	            bundles(where: { id: "1" }) {
+	                ethPrice
+	            }
+	        }
+	    `,
+	}
+	jsonValue, _ := json.Marshal(ethPrice)
+	request, err := http.NewRequest("POST", "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2", bytes.NewBuffer(jsonValue))
+	client := &http.Client{Timeout: time.Second * 10}
+	response, err := client.Do(request)
+	defer response.Body.Close()
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(data))
+}
