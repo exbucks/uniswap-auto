ee1621ce11257da6a45109d2d3ac233af3f34886
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-05 18:43:31 +0900

    add xi query

--- auto.go
+++ auto.go
@@ -10,16 +10,26 @@ import (
 )
 
 func main() {
-	ethPrice := map[string]string{
+	// ethPrice := map[string]string{
+	// 	"query": `
+	//         query bundles {
+	//             bundles(where: { id: "1" }) {
+	//                 ethPrice
+	//             }
+	//         }
+	//     `,
+	// }
+	xiQuery := map[string]string{
 		"query": `
-	        query bundles {
-	            bundles(where: { id: "1" }) {
-	                ethPrice
+	        query tokens {
+	            tokens(where: { id: "0x295b42684f90c77da7ea46336001010f2791ec8c" }) {
+	                derivedETH
+	                totalLiquidity
 	            }
 	        }
 	    `,
 	}
-	jsonValue, _ := json.Marshal(ethPrice)
+	jsonValue, _ := json.Marshal(xiQuery)
 	request, err := http.NewRequest("POST", "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2", bytes.NewBuffer(jsonValue))
 	client := &http.Client{Timeout: time.Second * 10}
 	response, err := client.Do(request)
